d003a152ff32a7b2e008baf83e018bf54f1be84f
BradlyKim <kjh5864@naver.com>
2022-03-09 17:16:11 +0900

    두번째 프래그먼트 작업  - 토스트는 작업 안 한 상태임.

--- app/src/main/java/com/neppplus/fragment_20220305/fragments/SecondFragment.kt
+++ app/src/main/java/com/neppplus/fragment_20220305/fragments/SecondFragment.kt
@@ -7,31 +7,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.fragment_20220305.R
-import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.android.synthetic.main.fragment_second.*
 
-class FirstFragment : Fragment() {
-
-//    fragment_first xml파일을, 모양으로 삼자
+class SecondFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_second, container, false)
     }
-
-//    fragment에서의 동작관련코드
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         
-        btnLogInFragment.setOnClickListener {
-            
-            Log.d("프래그먼트1","첫번째 프래그먼트에서 로그 찍기")
-            
-        }
-        
-    }
 
+    }
+    
 }
