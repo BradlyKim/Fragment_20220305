5b935a952fe508b1b7c8367754cf7b45a1957a19
BradlyKim <kjh5864@naver.com>
2022-03-09 17:19:32 +0900

    fragment에서 토스트 띄우기 - this 대신 requireContext() 함수 사용

--- app/src/main/java/com/neppplus/fragment_20220305/fragments/SecondFragment.kt
+++ app/src/main/java/com/neppplus/fragment_20220305/fragments/SecondFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.neppplus.fragment_20220305.R
 import kotlinx.android.synthetic.main.fragment_second.*
@@ -21,7 +22,12 @@ class SecondFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        
+
+        btnToastInFragment.setOnClickListener {
+
+            Toast.makeText(requireContext(), "토스트 띄우기", Toast.LENGTH_SHORT).show()
+
+        }
 
     }
     
