62ab15f093eb89935a5b240a71f28e61430240ad
BradlyKim <kjh5864@naver.com>
2022-03-09 16:53:11 +0900

    fragment 동작 관련 코드 함수 수정 onActivityCreated

--- app/src/main/java/com/neppplus/fragment_20220305/fragments/FirstFragment.kt
+++ app/src/main/java/com/neppplus/fragment_20220305/fragments/FirstFragment.kt
@@ -1,11 +1,13 @@
 package com.neppplus.fragment_20220305.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.fragment_20220305.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -23,6 +25,13 @@ class FirstFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        
+        btnLogInFragment.setOnClickListener { 
+            
+            Log.d("프래그먼트","첫번째 프래그먼트에서 로그 찍기")
+            
+        }
+        
     }
 
 }
